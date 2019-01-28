7d13a3509a807a5e43caf716312afe27ba8ce205
ekaterinamakarova <ekaterina.makarova.1999@mail.ru>
2019-01-28 17:07:47 +0300

    commit

--- src/test/java/Pages/CreateGroup.java
+++ src/test/java/Pages/CreateGroup.java
@@ -1,10 +1,13 @@
 package Pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class CreateGroup {
 
@@ -14,20 +17,28 @@ public class CreateGroup {
         PageFactory.initElements(driver,this);
     }
 
-    @FindBy(css="input[name='groups.name']") private WebElement addPartnetBtn;
-    @FindBy(css = "input[name='checkbox_63']") private  WebElement firstCheckBox;
-    @FindBy(xpath = "//span[contains(text(),'ADD PARTNER')]") private WebElement groupNameField;
+    @FindBy(xpath="//span[contains(text(),'ADD PARTNER')]") private WebElement addPartnerBtn;
+    @FindBy(css="tr[class] input") private List<WebElement> allChechboxes;
+    @FindBy(xpath = "//button[2]/span[1][contains(text(),'ADD PARTNER')]") private WebElement addPartnrBtn;
+    @FindBy(css = "button[type='submit']") private WebElement sendToPartnersBtn;
+    @FindBy(css = "input[name='groups.name']") private WebElement groupNameField;
 
 
     public void createGroupChecking() throws InterruptedException {
         Thread.sleep(1000);
-        scrollToElement(addPartnetBtn);
-        addPartnetBtn.click();
-        firstCheckBox.click();
+        scrollToElement(addPartnerBtn);
+        addPartnerBtn.click();
+        Thread.sleep(1000);
+        Assert.assertTrue(allChechboxes.size()!=0);
+        System.out.println(allChechboxes.size());
+        allChechboxes.get(0).click();
+        addPartnrBtn.click();
+        Thread.sleep(1000);
+        sendToPartnersBtn.click();
+        }
+
         //groupNameField.sendKeys(" ");
 
-
-    }
 
     private void scrollToElement(WebElement element){
         Actions actions=new Actions(driver);
